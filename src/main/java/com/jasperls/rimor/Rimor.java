d718b803b011e12dbaff2cb2ec576f2d1a7eed0f
Jasper Lutz Severino <jasperlutzseverino@gmail.com>
2022-01-03 01:07:37 +0100

    Implement recursive search and parameter support
    Signed-off-by: Jasper Lutz Severino <jasperlutzseverino@gmail.com>

--- src/main/java/com/jasperls/rimor/Rimor.java
+++ src/main/java/com/jasperls/rimor/Rimor.java
@@ -2,26 +2,22 @@ package com.jasperls.rimor;
 
 import com.jasperls.rimor.annotation.CommandNames;
 import com.jasperls.rimor.type.Command;
-import com.jasperls.rimor.type.MethodSubcommand;
 
-import java.lang.reflect.Method;
 import java.util.*;
 
 public class Rimor {
     private final Map<String, Command> commands = new HashMap<>();
 
-    /**
-     * Adds your desired {@link Command} object/s to a {@link Map} for access and evaluation.
-     *
-     * @param commands a single or multiple {@link Command} to register
-     */
     public void registerCommands(Command... commands) {
         for (Command command : commands) {
             List<String> aliases = new ArrayList<>();
 
-            if (command.getClass().isAnnotationPresent(CommandNames.class) && command.getClass().getAnnotation(CommandNames.class).value() != null)
-                aliases = Arrays.asList(command.getClass().getAnnotation(CommandNames.class).value());
-            else aliases.add(command.getClass().getSimpleName().toLowerCase());
+            CommandNames commandNames = command.getClass().getAnnotation(CommandNames.class);
+
+            if (commandNames != null && commandNames.value() != null)
+                Collections.addAll(aliases, commandNames.value());
+
+            aliases.add(command.getClass().getSimpleName().toLowerCase());
 
             for (String alias : aliases) {
                 this.commands.put(alias.toLowerCase(), command);
@@ -29,69 +25,58 @@ public class Rimor {
         }
     }
 
-    /**
-     * Gets a single {@link Command} using a linked name.
-     *
-     * @param name a command name
-     * @return an {@link Optional<Command>} that matches the provided name
-     */
     public Optional<Command> getCommand(String name) {
         return Optional.ofNullable(this.commands.get(name));
     }
 
-    /**
-     * Returns all saved {@link Command} objects regardless their name or properties.
-     *
-     * @return a {@link Collection} of all available {@link Command}
-     */
     public Collection<Command> getAllCommands() {
-        Collection<Command> commands = new HashSet<>();
-
-        for (Command command : this.commands.values()) {
-            if (!commands.contains(command)) {
-                commands.add(command);
-            }
-        }
-        return commands;
+        return commands.values();
     }
 
-    /**
-     * Uses a {@link String} array to find a {@link Method} that, hopefully, contains the desired code that needs to be run.
-     *
-     * @param path the command hierarchy path
-     * @return the evaluated final {@link Optional<Method>}
-     */
-    public Optional<Method> evaluate(String[] path) {
-        Command firstCommand;
+    public <T extends ExecutionData> void execute(String[] path, T data) {
+        data.setParameters(List.of(Arrays.copyOfRange(path, 1, path.length)));
 
-        if (getCommand(path[0]).isPresent()) firstCommand = getCommand(path[0]).get();
-        else return Optional.empty();
+        this.getCommand(path[0]).ifPresentOrElse(
+                command -> this.execute(command, data),
+                () -> {
+                    throw new NullPointerException("No command named \"" + path[0] + "\" was found.");
+                }
+        );
+    }
 
-        if (firstCommand.getLonelyMethod() != null) return Optional.of(firstCommand.getLonelyMethod());
+    void execute(Command command, ExecutionData data) {
+        List<String> path = data.getParameters();
 
-        MethodSubcommand methodSubcommand;
-        boolean isNested = false;
-        String nestedStep = null;
-
-        for (String step : path) {
-            methodSubcommand = firstCommand.getSubcommandMap().get(step);
-
-            if (methodSubcommand != null) {
-                return Optional.of(methodSubcommand.getMethod());
-            }
-
-            if (firstCommand.getChildCommandMap().get(step) != null) {
-                isNested = true;
-                nestedStep = step;
-                continue;
-            }
-
-            methodSubcommand = firstCommand.getChildCommandMap().get(nestedStep).getSubcommandMap().get(step);
-
-            if (isNested && methodSubcommand != null) {
-                return Optional.of(methodSubcommand.getMethod());
-            }
+        if (path.isEmpty()) {
+            command.getCommandMethod().ifPresentOrElse(
+                    commandMethod -> commandMethod.invoke(command, data),
+                    () -> {
+                        throw new NullPointerException("No method command was found.");
+                    }
+            );
+            return;
         }
-        return Optional.empty();
+
+        command.getSubcommandMethod(path.get(0)).ifPresentOrElse(
+                subcommandMethod -> {
+                    data.setParameters(path.subList(1, path.size()));
+                    subcommandMethod.invoke(command, data);
+                },
+                () -> command.getSubcommandGroup(path.get(0)).ifPresentOrElse(
+                        subcommandGroup -> {
+                            data.setParameters(path.subList(1, path.size()));
+                            execute(subcommandGroup, data);
+                        },
+                        () -> command.getCommandMethod().ifPresentOrElse(
+                                commandMethod -> {
+                                    data.setParameters(path.subList(1, path.size()));
+                                    commandMethod.invoke(command, data);
+                                },
+                                () -> {
+                                    throw new NullPointerException("No subcommand method, subcommand group or command method found for \"" + path.get(0) + "\".");
+                                }
+                        )
+                )
+        );
     }
 }
