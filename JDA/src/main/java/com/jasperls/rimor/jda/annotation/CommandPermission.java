e51ee20a9ef311fb760ca6d51beb3492b6e32429
Jasper Lutz Severino <jasperlutzseverino@gmail.com>
2022-01-05 03:35:57 +0100

    Add annotated permissions
    Signed-off-by: Jasper Lutz Severino <jasperlutzseverino@gmail.com>

---
+++ JDA/src/main/java/com/jasperls/rimor/jda/annotation/CommandPermission.java
@@ -0,0 +1,14 @@
+package com.jasperls.rimor.jda.annotation;
+
+import net.dv8tion.jda.api.Permission;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.METHOD, ElementType.TYPE})
+public @interface CommandPermission {
+    Permission[] value();
+}
