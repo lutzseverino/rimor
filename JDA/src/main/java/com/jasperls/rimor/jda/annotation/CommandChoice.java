0f4a1c8682cea725d71d16a4e90df0441762f43c
Jasper Lutz Severino <jasperlutzseverino@gmail.com>
2022-01-05 03:35:36 +0100

    Add annotated Option structure
    Signed-off-by: Jasper Lutz Severino <jasperlutzseverino@gmail.com>

--- JDA/src/main/java/com/jasperls/rimor/jda/annotation/CommandChoice.java
+++ JDA/src/main/java/com/jasperls/rimor/jda/annotation/CommandChoice.java
@@ -1,20 +1,13 @@
 package com.jasperls.rimor.jda.annotation;
 
-import net.dv8tion.jda.api.interactions.commands.OptionType;
-
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
-@Target({ElementType.METHOD, ElementType.TYPE})
-public @interface DeclareOption {
-    OptionType type();
-
+@Target(ElementType.ANNOTATION_TYPE)
+public @interface CommandChoice {
     String name();
-
-    String description();
-
-    boolean required();
+    String value();
 }
