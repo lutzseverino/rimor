e6105e20dbe188416cd6334a09462058bc358d68
Jasper Lutz Severino <jasperlutzseverino@gmail.com>
2022-01-06 17:26:49 +0100

    Remove impl package inside Core method
    Signed-off-by: Jasper Lutz Severino <jasperlutzseverino@gmail.com>

---
+++ Core/src/main/java/com/jasperls/rimor/method/CommandMethod.java
@@ -0,0 +1,10 @@
+package com.jasperls.rimor.method;
+
+import java.lang.reflect.Method;
+
+public class CommandMethod extends RimorMethod {
+
+    public CommandMethod(Method method) {
+        super(method);
+    }
+}
