26cca4cc9be6129db03f062061f701857a80ce18
Jasper Lutz Severino <jasperlutzseverino@gmail.com>
2022-01-10 00:49:50 +0100

    Add lombok setter and remove redundant constructor
    Signed-off-by: Jasper Lutz Severino <jasperlutzseverino@gmail.com>

--- Core/src/main/java/com/jasperls/rimor/ExecutionData.java
+++ Core/src/main/java/com/jasperls/rimor/ExecutionData.java
@@ -1,18 +1,13 @@
 package com.jasperls.rimor;
 
 import lombok.Getter;
+import lombok.Setter;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Getter
+@Setter
 public class ExecutionData {
     public List<String> parameters = new ArrayList<>();
-
-    public ExecutionData() {
-    }
-    
-    public void setParameters(List<String> params) {
-        this.parameters = params;
-    }
 }
