3ab3be402fd37cf6985b979ca5ef90aee764c338
Jasper Lutz Severino <jasperlutzseverino@gmail.com>
2022-01-10 20:29:36 +0100

    Fix GuildJoinListener, only guild commands are updated

--- JDA/src/main/java/com/jasperls/rimor/jda/event/GuildJoinListener.java
+++ JDA/src/main/java/com/jasperls/rimor/jda/event/GuildJoinListener.java
@@ -1,20 +1,28 @@
 package com.jasperls.rimor.jda.event;
 
 import com.jasperls.rimor.Rimor;
-import com.jasperls.rimor.jda.manager.JDARimorManager;
+import com.jasperls.rimor.jda.manager.JDAManager;
 import com.jasperls.rimor.jda.type.JDACommand;
 import com.jasperls.rimor.type.Command;
 import net.dv8tion.jda.api.events.guild.GuildJoinEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class GuildJoinListener extends ListenerAdapter {
     @Override
     public void onGuildJoin(@NotNull GuildJoinEvent event) {
-        JDARimorManager jdaRimorManager = new JDARimorManager(event.getJDA());
+        JDAManager jdaManager = new JDAManager(event.getJDA());
+
+        List<JDACommand> jdaCommandList = new ArrayList<>();
 
         for (Command command : Rimor.INSTANCE.getAllCommands())
             if (command instanceof JDACommand jdaCommand && jdaCommand.isGuildOnlySetting())
-                jdaRimorManager.registerCommands(event.getGuild());
+                jdaCommandList.add(jdaCommand);
+
+        jdaManager.uploadCommands(event.getGuild(), jdaCommandList);
+
     }
 }
