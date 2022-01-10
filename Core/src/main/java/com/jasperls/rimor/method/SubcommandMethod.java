a1efd5143d0dc4c3c05df120fffb810402c863f9
Jasper Lutz Severino <jasperlutzseverino@gmail.com>
2022-01-10 01:48:45 +0100

    Remove parent property of SubcommandMethod.java
    Signed-off-by: Jasper Lutz Severino <jasperlutzseverino@gmail.com>

--- Core/src/main/java/com/jasperls/rimor/method/SubcommandMethod.java
+++ Core/src/main/java/com/jasperls/rimor/method/SubcommandMethod.java
@@ -1,16 +1,13 @@
 package com.jasperls.rimor.method;
 
-import com.jasperls.rimor.type.Command;
 import lombok.Getter;
 
 import java.lang.reflect.Method;
 
 @Getter
 public class SubcommandMethod extends RimorMethod {
-    private final Command parent;
 
-    public SubcommandMethod(Method method, Command parent) {
+    public SubcommandMethod(Method method) {
         super(method);
-        this.parent = parent;
     }
 }
