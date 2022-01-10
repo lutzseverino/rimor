18aa8c5ccb02962a54b536a0a8ec0317de816473
Jasper Lutz Severino <jasperlutzseverino@gmail.com>
2022-01-10 14:12:07 +0100

    Add JDACommandMethod#getOptionMethods
    Signed-off-by: Jasper Lutz Severino <jasperlutzseverino@gmail.com>

--- JDA/src/main/java/com/jasperls/rimor/jda/method/JDACommandMethod.java
+++ JDA/src/main/java/com/jasperls/rimor/jda/method/JDACommandMethod.java
@@ -3,11 +3,13 @@ package com.jasperls.rimor.jda.method;
 import com.jasperls.rimor.method.CommandMethod;
 
 import java.lang.reflect.Method;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class JDACommandMethod extends CommandMethod {
-    public final Map<String, OptionMethod> optionMethodMap = new HashMap<>();
+    private final Map<String, OptionMethod> optionMethodMap = new HashMap<>();
 
     public JDACommandMethod(Method method) {
         super(method);
@@ -19,5 +21,9 @@ public class JDACommandMethod extends CommandMethod {
 
     public OptionMethod getOptionMethod(String name) {
         return this.optionMethodMap.get(name);
+    }
+
+    public List<OptionMethod> getOptionMethods() {
+        return new ArrayList<>(this.optionMethodMap.values());
     }
 }
