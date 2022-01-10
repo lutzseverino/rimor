fb57acbcb1808529e36dee6b024879462b5a38f0
Jasper Lutz Severino <jasperlutzseverino@gmail.com>
2022-01-10 19:49:27 +0100

    Document decoration classes

--- Core/src/main/java/com/jasperls/rimor/type/SubcommandGroup.java
+++ Core/src/main/java/com/jasperls/rimor/type/SubcommandGroup.java
@@ -1,5 +1,10 @@
 package com.jasperls.rimor.type;
 
+/**
+ * Decoration class to declare subcommands that can hold options,
+ * has no real logic or extra functionality, its only purpose
+ * is to make the users' code easier to understand.
+ */
 public abstract class SubcommandGroup extends Command {
 
 }
--- JDA/src/main/java/com/jasperls/rimor/jda/type/OptionSubcommand.java
+++ JDA/src/main/java/com/jasperls/rimor/jda/type/OptionSubcommand.java
@@ -1,5 +1,10 @@
 package com.jasperls.rimor.jda.type;
 
+/**
+ * Decoration class to declare subcommands that can hold options,
+ * has no real logic or extra functionality, its only purpose
+ * is to make the users' code easier to understand.
+ */
 public class OptionSubcommand extends JDACommand {
 
 }
