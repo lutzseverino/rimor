b7e7cdd5294f1ba996503f6bf2948b23a31aebd4
Jasper Lutz Severino <jasperlutzseverino@gmail.com>
2022-01-26 20:14:22 +0100

    Remove forgotten TODO

--- JDA/src/main/java/com/jasperls/rimor/jda/JDAManager.java
+++ JDA/src/main/java/com/jasperls/rimor/jda/JDAManager.java
@@ -3,7 +3,6 @@ package com.jasperls.rimor.jda;
 import com.jasperls.rimor.jda.event.SlashCommandListener;
 import net.dv8tion.jda.api.JDA;
 
-// TODO Rework manager, make methods more implementable and fix
 public class JDAManager {
     private final JDA jdaInstance;
 
