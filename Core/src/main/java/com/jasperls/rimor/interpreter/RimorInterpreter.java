e0f884c68681adf9d9f5e3dddfca823056a6ad57
Jasper Lutz Severino <jasperlutzseverino@gmail.com>
2022-02-27 02:39:12 +0100

    Clean JDAInterpreter Trust user on data instancing.
    Signed-off-by: Jasper Lutz Severino <jasperlutzseverino@gmail.com>

--- Core/src/main/java/com/jasperls/rimor/interpreter/RimorInterpreter.java
+++ Core/src/main/java/com/jasperls/rimor/interpreter/RimorInterpreter.java
@@ -1,19 +1,19 @@
 package com.jasperls.rimor.interpreter;
 
-import com.jasperls.rimor.data.ExecutionData;
+import com.jasperls.rimor.data.Data;
 import com.jasperls.rimor.method.RimorMethod;
 
 import java.util.Optional;
 
 public interface RimorInterpreter {
 
-    void execute(String[] path, ExecutionData data);
+    void execute(String[] path, Data data);
 
     /**
      * @param path a path with each individual command steps
-     * @param data a data source that is, or extends {@link ExecutionData}
+     * @param data a data source that is, or extends {@link Data}
      * @return the final {@link RimorMethod}
      * @throws IllegalArgumentException if no command is found
      */
-    Optional<? extends RimorMethod> findMethod(String[] path, ExecutionData data);
+    Optional<? extends RimorMethod> findMethod(String[] path, Data data);
 }
--- JDA/src/main/java/com/jasperls/rimor/jda/interpreter/JDAInterpreter.java
+++ JDA/src/main/java/com/jasperls/rimor/jda/interpreter/JDAInterpreter.java
@@ -1,17 +1,16 @@
 package com.jasperls.rimor.jda.interpreter;
 
 import com.jasperls.rimor.Rimor;
-import com.jasperls.rimor.data.ExecutionData;
+import com.jasperls.rimor.data.Data;
 import com.jasperls.rimor.interpreter.CoreInterpreter;
 import com.jasperls.rimor.interpreter.RimorInterpreter;
-import com.jasperls.rimor.jda.data.JDAExecutionData;
+import com.jasperls.rimor.jda.data.JDACommandData;
 import com.jasperls.rimor.jda.data.OptionExecutionData;
 import com.jasperls.rimor.jda.method.JDACommandMethod;
 import com.jasperls.rimor.jda.method.OptionMethod;
 import com.jasperls.rimor.jda.type.JDACommand;
 import com.jasperls.rimor.method.RimorMethod;
 import com.jasperls.rimor.type.Command;
-import net.dv8tion.jda.api.interactions.commands.OptionMapping;
 
 import java.util.Arrays;
 import java.util.List;
@@ -19,30 +18,26 @@ import java.util.Optional;
 
 public class JDAInterpreter implements RimorInterpreter {
 
-    private JDACommand commandInstance;
+    private JDACommand instance;
 
-    @Override public void execute(String[] path, ExecutionData data) {
-        if (data instanceof JDAExecutionData jdaData) {
-            this.findMethod(path, data).ifPresentOrElse(method -> {
+    @Override public void execute(String[] path, Data data) {
+        this.findMethod(path, data).ifPresentOrElse(method -> {
 
-                if (method instanceof JDACommandMethod jdaMethod) {
-                    for (OptionMapping option : jdaData.getEvent().getOptions()) {
-                        OptionMethod optionMethod = jdaMethod.getOptionMethod(option.getName());
-                        optionMethod.invoke(commandInstance, new OptionExecutionData(option));
-                    }
-                }
+            if (method instanceof JDACommandMethod jdaMethod && data instanceof JDACommandData jdaData) {
+                jdaData.getEvent().getOptions().forEach(option -> {
+                    OptionMethod optionMethod = jdaMethod.getOptionMethod(option.getName());
+                    optionMethod.invoke(this.instance, new OptionExecutionData(option));
+                });
+            }
 
-                method.invoke(commandInstance, jdaData);
+            method.invoke(this.instance, data);
 
-            }, () -> {
-                throw new IllegalArgumentException("Couldn't find method to execute with path " + Arrays.toString(path));
-            });
-        } else {
-            throw new IllegalArgumentException("Data is missing properties due to wrong instancing");
-        }
+        }, () -> {
+            throw new IllegalArgumentException("Couldn't find method to execute with path " + Arrays.toString(path));
+        });
     }
 
-    @Override public Optional<? extends RimorMethod> findMethod(String[] path, ExecutionData data) {
+    @Override public Optional<? extends RimorMethod> findMethod(String[] path, Data data) {
         data.setParameters(List.of(Arrays.copyOfRange(path, 1, path.length)));
 
         Optional<Command> command = Rimor.INSTANCE.getCommand(path[0]);
@@ -57,17 +52,17 @@ public class JDAInterpreter implements RimorInterpreter {
      * Recursively looks for a {@link RimorMethod}.
      *
      * @param command the {@link Command} for which to look recursively in for
-     * @param data    a data source that is, or extends {@link ExecutionData}
+     * @param data    a data source that is, or extends {@link Data}
      * @return the final {@link RimorMethod}
      * @throws IllegalArgumentException if no command method, subcommand method or subcommand group is found
-     * @see CoreInterpreter#findMethod(String[], ExecutionData)
+     * @see CoreInterpreter#findMethod(String[], Data)
      */
-    public Optional<? extends RimorMethod> findMethod(Command command, ExecutionData data) {
+    public Optional<? extends RimorMethod> findMethod(Command command, Data data) {
         List<String> path = data.getParameters();
         RimorMethod method = null;
 
         if (command instanceof JDACommand jdaCommand) {
-            this.commandInstance = jdaCommand;
+            this.instance = jdaCommand;
 
             if (path.isEmpty()) {
                 method = command.getCommandMethod();
@@ -77,9 +72,10 @@ public class JDAInterpreter implements RimorInterpreter {
                 method = jdaCommand.getSubcommandMethod(path.get(0));
 
             } else if (jdaCommand.getOptionSubcommand(path.get(0)) != null) {
-                this.commandInstance = jdaCommand.getOptionSubcommand(path.get(0));
+                this.instance = jdaCommand.getOptionSubcommand(path.get(0));
+
                 data.setParameters(path.subList(1, path.size()));
-                method = this.commandInstance.getJdaCommandMethod();
+                method = jdaCommand.getJdaCommandMethod();
 
             } else if (jdaCommand.getSubcommandGroup(path.get(0)) != null) {
                 data.setParameters(path.subList(1, path.size()));
